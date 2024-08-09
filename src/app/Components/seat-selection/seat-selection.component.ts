6326df4d0ac49dab1cc1b1ff78db18750025927e
“devarshi” <devarshikathiriya@gmail.com>
2024-08-09 18:59:07 +0530

    light-dark-day3

---
+++ src/app/Components/seat-selection/seat-selection.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-seat-selection',
+  templateUrl: './seat-selection.component.html',
+  styleUrls: ['./seat-selection.component.scss']
+})
+export class SeatSelectionComponent  {
+
+}
